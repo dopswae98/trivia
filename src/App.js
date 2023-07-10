19a08063ae31100ef9c315f66a6ec52e9501641f
dopswae98 <ephraimmatarutse@gmail.com>
2023-07-10 13:46:45 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Nimbish from "./Pages/Nimbish";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import "font-awesome/css/font-awesome.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Nimbish />
+    </div>
+  );
+}
+
+export default App;
